ae6dbdf87b68aee13eeab783a6ce51bde36672df
padzoid <>
2021-10-11 01:35:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#ODk2OTIzNjA3NzkwNDAzNTg0.YWOLNA.PB_02PT1lnDk7dIVWzj8_V1RIq8
+import discord 
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '>')
+
+@client.event
+async def on_ready():
+  print("The bot is online!")
+  
+@client.command()
+async def ping(ctx):
+  await ctx.send("Pong! :ping_pong:")
+
+client.run("ODk2OTIzNjA3NzkwNDAzNTg0.YWOLNA.PB_02PT1lnDk7dIVWzj8_V1RIq8
